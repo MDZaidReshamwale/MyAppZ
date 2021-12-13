4bf18a57104d432a5375a978d5697fac0817307f
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-13 15:42:43 +0530

    Onclick EventHandler and language change

--- app/src/main/java/com/example/myappz/MainActivity.java
+++ app/src/main/java/com/example/myappz/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myappz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void showToastMessage(View view) {
+        //Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.message, Toast.LENGTH_LONG).show();
+
+        add(10,20);
+    }
+
+    int add(int fnum, int snum){
+        return fnum + snum;
     }
 }
