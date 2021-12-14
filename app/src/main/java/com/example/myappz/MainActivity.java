c3a6a34375903e403b87c9122ccf102625bd7c74
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-14 12:35:49 +0530

    Sending and reading data from one activity and anther activity.

--- app/src/main/java/com/example/myappz/MainActivity.java
+++ app/src/main/java/com/example/myappz/MainActivity.java
@@ -5,33 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameEditText; //declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameEditText = findViewById(R.id.UName); //initialization
     }
 
 
     public void showToastMessage(View view) {
         //Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, R.string.message, Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, R.string.message, Toast.LENGTH_LONG).show();
+        String name = nameEditText.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
 
-        add(10,20);
+        Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+        i.putExtra("waterkey",name);
+        startActivity(i);
+
+        add(10, 20);
     }
 
-    int add(int fnum, int snum){
+    int add(int fnum, int snum) {
         return fnum + snum;
     }
 
-    public void nextActivity(View v)
-    {
+    public void nextActivity(View v) {
         // TODO Auto-generated method stub
-        Intent i = new Intent(getApplicationContext(),MainActivity2.class);
-        startActivity(i);
+
 
     }
 }
--- app/src/main/java/com/example/myappz/MainActivity2.java
+++ app/src/main/java/com/example/myappz/MainActivity2.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,14 +14,22 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView homeTextView = findViewById(R.id.display);
+        String name = getIntent().getExtras().getString("waterkey");
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        homeTextView.setText(name);
     }
+
+
     public  void Design (View view){
         Intent i = new Intent(getApplicationContext(),Design.class);
         startActivity(i);
     }
 
     public void Wewe(View view) {
+
         Intent i = new Intent(getApplicationContext(),Design.class);
         startActivity(i);
+
     }
 }
